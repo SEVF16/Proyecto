b7399deefa243471ede507fae7768c2643e7f1c2
Shiryu Valecillos <shiryuevfr@gmail.com>
2021-09-13 22:23:43 -0300

    primer commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,14 +5,20 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
   imports: [
+    MatFormFieldModule,
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    MatButtonModule,
+    MatCardModule,
   ],
   declarations: [HomePage]
 })
