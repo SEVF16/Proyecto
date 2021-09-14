b03cb10850b5ec1fa8aabe8fdaf6a1cc765b9c5c
carCorrea <carlos.correa.zapata@gmail.com>
2021-09-14 20:49:31 -0300

    (Carlos) Added password recovery link and page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: 'landing',
     loadChildren: () => import('./landing/landing.module').then( m => m.LandingPageModule)
   },
+  {
+    path: 'pass-reset',
+    loadChildren: () => import('./pass-reset/pass-reset.module').then( m => m.PassResetPageModule)
+  },
 
 ];
 
