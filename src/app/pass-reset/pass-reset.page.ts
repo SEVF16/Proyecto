f156f58fa85f54bfb0f7c9d6accf5d326bde345c
Shiryu Valecillos <shiryuevfr@gmail.com>
2021-09-15 22:47:40 -0300

    Page de recuperar contraseña

--- src/app/pass-reset/pass-reset.page.ts
+++ src/app/pass-reset/pass-reset.page.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pass-reset',
   templateUrl: './pass-reset.page.html',
   styleUrls: ['./pass-reset.page.scss'],
 })
-export class PassResetPage implements OnInit {
-
-  constructor() { }
+export class PassResetPage{
+  user={
+    userName:"",
+  }
+  constructor(public toastController: ToastController, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  recuperar() {
+    let navigationExtras: NavigationExtras ={
+      state:{user: this.user.userName}
+    };
+
+    if (this.user.userName == ""){
+      this.show();
+    }
+    else{
+      this.router.navigate(['/home'], navigationExtras);
+    }    
+  };
+
+  show(){
+    this.showData("Debe ingresar su nombre de usuario");
+  }
+
+  async showData(msg: string){
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
